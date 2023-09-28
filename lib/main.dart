32358eb82649b1122fba6b5c9da8c08141a6a406
“roon-replica” <think-roon@naver.com>
2023-09-28 20:20:49 +0900

    첫번째 앱 튜토리얼 - 버튼 누르면 다른 랜덤 문자열 표시

--- lib/main.dart
+++ lib/main.dart
@@ -26,20 +26,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 앱의 상태 정의?
+// ChangeNotifier가 앱 상태 관리하는 가장 쉬운 방법이래
+// MyAppState: 앱에 필요한 데이터 정의, 리액트의 useState같은 훅 개념인듯
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    var appState = context.watch<MyAppState>(); // watch: 변경사항 추적. react-hook-form이랑 비슷. hook 개념으로 보면될듯
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('button')
+          )
         ],
       ),
     );
